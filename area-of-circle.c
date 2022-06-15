74dd5a6b31836355eac56f430d23efac0fe371c8
Rohit Thakur <105488212+its-me-rohit@users.noreply.github.com>
2022-06-15 16:01:33 +0530

    Create area-of-circle.c

---
+++ area-of-circle.c
@@ -0,0 +1,16 @@
+// To fing the area of the circle 
+
+#include<stdio.h>
+
+int main(){
+    float radius;
+    scanf("%f",&radius) ;
+    printf("%f \n ",3.14*radius*radius);
+    
+    
+    return 0 ;
+    
+    
+
+
+}
